5587883427fcbe187e4fc675f2ff365850c74a60
sid597 <siddharthdv77@gmail.com>
2020-08-30 01:35:22 +0530

    directory renamed and removed a file

--- Exercises_from_theBook/vector_exercise/src/main.rs
+++ Exercises_from_theBook/vector_exercise/src/main.rs
@@ -5,12 +5,13 @@ Given a list of integers, use a vector and return the mean (the average value), 
 use std::collections::HashMap;
 
 fn main() {
-    let mut v = vec![32,2, 4, 65,24, 54, 23,77, 74,5 ,8, 3, 47,85];
+    let mut v = vec![32,2,22,2,2,2, 4, 65,24, 54, 23,77, 74,5 ,8, 3, 47,85];
     v.sort();
     println!("Vector v is {:?}", v);
     println!("Vector mean is {}",mean(&v));
     
     println!("Vector mean is {}",median(&v));
+    println!("Vector mode is {}", mode(&v));
 }
 
 fn mean(v:&Vec<u64>) -> u64{
@@ -28,3 +29,22 @@ fn median(v:&Vec<u64>) -> u64 {
     v[v.len()-1] 
 }
 
+fn mode(v:&Vec<u64>) -> u64 {
+
+    let mut hm = HashMap::new();
+    let mut mx = 0;
+    let mut mx_val:u64=0;
+    
+    for n in v {
+        let count = hm.entry(n).or_insert(0);
+        *count +=1;
+    }
+
+    for ki in hm{
+        if ki.1 > mx {
+            mx = ki.1;
+            mx_val = *ki.0;
+        }
+    }
+    mx_val
+}
